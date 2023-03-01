fbeb4e96725d0024d721de5f271b63333da4837a
Beltranax <beltranangel7293@gmail.com>
2023-03-01 23:14:47 +0000

    lab completed

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class CombineArrays {
     /**
@@ -8,7 +9,15 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int a1 = arr1.length;
+        int a2 = arr2.length;
 
-        return null;
+        int a3 = a1 + a2;
+
+        int [] arr3 = new int[a3];
+
+        System.arraycopy(arr1, 0, arr3, 0, a1);
+        System.arraycopy(arr2, 0, arr3, a1, a2);
+        return arr3;
     }
 }
